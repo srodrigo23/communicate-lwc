e41dae62b5bfdae6800a4564e17161d72fb53976
Sergio Cardenas <sergio.cardenas@oktana.com>
2022-01-27 23:18:03 -0400

    use a public getter and setter

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -6,7 +6,23 @@ export default class Numerator extends LightningElement {
     The @api decorator in the child component exposes a property, making it public, 
     so that the parent component can update it.
     */
-    @api counter = 0;
+    //@api counter = 0;
+
+    /*
+    This code changes counter to a property with getter (get) and setter (set) functions. 
+    It also adds the priorCount and _currentCount properties.
+    */
+    _currentCount = 0;
+    priorCount = 0;
+    @api
+    get counter() {
+        return this._currentCount;
+    }
+
+    set counter(value) {
+        his.priorCount = this._currentCount;
+        this._currentCount = value;
+    }
 
     handleIncrement() {
         this.counter++;
